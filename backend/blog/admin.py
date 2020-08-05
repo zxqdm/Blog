585dd93d24518a11b15fc6be2b0b2e95a7b528a1
姚彦鑫 <997635832@qq.com>
2020-08-05 13:34:36 +0800

    del

--- backend/blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from blog.models import Blog, Tag, Comment, Reply
-# Register your models here.
-
-
-admin.site.register([Blog, Tag, Comment, Reply])
--- backend/user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from user import models
-# Register your models here.
-
-
-admin.site.register(models.User)
